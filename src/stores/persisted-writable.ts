ba8ae3fcb64bfe364e46f017360ce36790f350c3
Simon Mudd <smudd@amplience.com>
2020-11-12 08:31:57 +0000

    fix(persisted writable): changed from using localstorage to use sessionstorage

--- src/stores/persisted-writable.ts
+++ src/stores/persisted-writable.ts
@@ -5,15 +5,15 @@ export const PREFIX = 'dc-dashboard-ga.';
 export function persistedWritable<T>(key: string, value: T): Writable<T> {
   const svelteStore = svelteWritable(value);
 
-  const localStorageKey = PREFIX + key;
+  const storageKey = PREFIX + key;
 
-  const newValue = localStorage.getItem(localStorageKey);
+  const newValue = sessionStorage.getItem(storageKey);
   if (newValue) {
     svelteStore.set(JSON.parse(newValue));
   }
 
   svelteStore.subscribe((value) => {
-    localStorage.setItem(localStorageKey, JSON.stringify(value));
+    sessionStorage.setItem(storageKey, JSON.stringify(value));
   });
 
   return svelteStore;
