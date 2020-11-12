afa8c3aa16b3351b906a435616f4ffe8e2dcbb54
Simon Mudd <smudd@amplience.com>
2020-11-12 11:48:57 +0000

    fix(persisted writable): now subscribes to hub so storage is wrote when hub is defined

--- src/stores/persisted-writable.ts
+++ src/stores/persisted-writable.ts
@@ -1,20 +1,25 @@
-import { get, writable as svelteWritable, Writable } from 'svelte/store';
+import { writable as svelteWritable, Writable } from 'svelte/store';
 import { hub } from './dynamic-content';
 
 export const PREFIX = 'dc-dashboard-ga.';
 
 export function persistedWritable<T>(key: string, value: T): Writable<T> {
   const svelteStore = svelteWritable(value);
+  hub.subscribe((selectedHub) => {
+    if (selectedHub === null) {
+      return;
+    }
 
-  const localStorageKey = `${PREFIX}${get(hub)?.name}-${key}`;
+    const localStorageKey = `${PREFIX}${selectedHub?.name}-${key}`;
 
-  const newValue = localStorage.getItem(localStorageKey);
-  if (newValue) {
-    svelteStore.set(JSON.parse(newValue));
-  }
+    const newValue = localStorage.getItem(localStorageKey);
+    if (newValue) {
+      svelteStore.set(JSON.parse(newValue));
+    }
 
-  svelteStore.subscribe((value) => {
-    localStorage.setItem(localStorageKey, JSON.stringify(value));
+    svelteStore.subscribe((value) => {
+      localStorage.setItem(localStorageKey, JSON.stringify(value));
+    });
   });
 
   return svelteStore;
