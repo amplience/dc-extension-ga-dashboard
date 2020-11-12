2097cc117bb8749d77c7868203a67646dd41093c
Simon Mudd <smudd@amplience.com>
2020-11-12 09:00:58 +0000

    fix(persisted writable): changed the prefix to use the selected hub when storing persisted data

--- src/stores/persisted-writable.ts
+++ src/stores/persisted-writable.ts
@@ -1,11 +1,12 @@
-import { writable as svelteWritable, Writable } from 'svelte/store';
+import { get, writable as svelteWritable, Writable } from 'svelte/store';
+import { hub } from './dynamic-content';
 
 export const PREFIX = 'dc-dashboard-ga.';
 
 export function persistedWritable<T>(key: string, value: T): Writable<T> {
   const svelteStore = svelteWritable(value);
 
-  const localStorageKey = PREFIX + key;
+  const localStorageKey = `${PREFIX}${get(hub)?.name}-${key}`;
 
   const newValue = localStorage.getItem(localStorageKey);
   if (newValue) {
