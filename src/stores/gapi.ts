4a76a119448232d611fe904bbe997249f5a963bd
Ben Pearey <bpearey@amplience.com>
2020-11-25 10:33:24 +0000

    chore(gapi): removing console log

--- src/stores/gapi.ts
+++ src/stores/gapi.ts
@@ -8,7 +8,6 @@ export const initGapi = (): Promise<void> => {
   return new Promise((resolve, reject) => {
     const gaEmbedApi: GoogleAnalyticsEmbedAPI = window['gapi'] || undefined;
     if (!gaEmbedApi) {
-      console.log('hihi');
       reject(new Error('Unable to initialize gapi: missing gapi instance'));
     }
     gaEmbedApi.analytics.ready(function () {
