9412b525b4461db18b794b1c1bc166a667bbf990
Chris Bibby <50444773+c-bibby@users.noreply.github.com>
2020-12-08 16:14:42 +0000

    (ux tweaks): remove grey background and animated links (#61)
    * fix(dashboard tweaks): further changes to the dashboard ux
    
    * fix(dashboard tweaks): removed grey background and animated link on content-item-picker

--- src/components/content-item-picker/content-item-picker.svelte
+++ src/components/content-item-picker/content-item-picker.svelte
@@ -115,6 +115,25 @@
     background-color: transparent;
     border-bottom: 2px solid #42a5f5;
   }
+
+  :global(.mdc-list:not(.mdc-list--non-interactive)
+      > :not(.mdc-list-item--disabled).mdc-list-item::before) {
+    background-color: transparent;
+  }
+
+  :global(.mdc-list:not(.mdc-list--non-interactive)
+      > :not(.mdc-list-item--disabled).mdc-list-item.mdc-ripple-upgraded--foreground-activation::before, .mdc-list:not(.mdc-list--non-interactive)
+      > :not(.mdc-list-item--disabled).mdc-list-item.mdc-ripple-upgraded--foreground-activation::after) {
+    background-color: transparent;
+    transition: none;
+  }
+
+  :global(.mdc-list:not(.mdc-list--non-interactive)
+      > :not(.mdc-list-item--disabled).mdc-list-item.mdc-ripple-upgraded--foreground-deactivation::before, .mdc-list:not(.mdc-list--non-interactive)
+      > :not(.mdc-list-item--disabled).mdc-list-item.mdc-ripple-upgraded--foreground-deactivation::after) {
+    background-color: transparent;
+    transition: none;
+  }
 </style>
 
 <div class="container">
